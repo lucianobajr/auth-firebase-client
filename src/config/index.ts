e043090c2187d0732d51ba38e23fb2181d4765a1
lucianobajr <luciano.alcantara@ufv.br>
2022-02-08 11:04:17 -0300

    add

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,16 +7,4 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
 };
 
-/*
-const firebaseConfig = {
-  apiKey: "AIzaSyCMW-ocedOd-J2zhzNLE6_nBzi3DUrviOM",
-  authDomain: "auth-firebase-c1c6c.firebaseapp.com",
-  projectId: "auth-firebase-c1c6c",
-  storageBucket: "auth-firebase-c1c6c.appspot.com",
-  messagingSenderId: "51600224755",
-  appId: "1:51600224755:web:00d4e7eb3913316eda6286",
-};
-
-*/
-
 export default firebaseConfig;
