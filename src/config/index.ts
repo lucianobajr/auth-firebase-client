a2889ac8d4fb204915733707916a879b9e4d8625
lucianobajr <luciano.alcantara@ufv.br>
2022-02-08 10:57:10 -0300

    add

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,19 +1,20 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCMW-ocedOd-J2zhzNLE6_nBzi3DUrviOM",
-  authDomain: "auth-firebase-c1c6c.firebaseapp.com",
-  projectId: "auth-firebase-c1c6c",
-  storageBucket: "auth-firebase-c1c6c.appspot.com",
-  messagingSenderId: "51600224755",
-  appId: "1:51600224755:web:00d4e7eb3913316eda6286",
-};
-/*
-const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
+};
+
+/*
+const firebaseConfig = {
+  apiKey: "AIzaSyCMW-ocedOd-J2zhzNLE6_nBzi3DUrviOM",
+  authDomain: "auth-firebase-c1c6c.firebaseapp.com",
+  projectId: "auth-firebase-c1c6c",
+  storageBucket: "auth-firebase-c1c6c.appspot.com",
+  messagingSenderId: "51600224755",
+  appId: "1:51600224755:web:00d4e7eb3913316eda6286",
 };
 
 */
